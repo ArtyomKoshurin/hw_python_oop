6979971f5d0db86cda7e1ffaab1b008a8d325f67
Артём <coshurin.art@yandex.ru>
2023-01-15 15:19:03 +0300

    final version

--- homework.py
+++ homework.py
@@ -7,7 +7,7 @@ class InfoMessage:
                  distance: float,
                  speed: float,
                  calories: float
-                 ) -> str:
+                 ) -> None:
         self.training_type = training_type
         self.duration = duration
         self.distance = distance
