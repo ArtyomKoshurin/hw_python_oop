3965d587c57a16894fb9fb1f9a8ef30f49cdaa9f
Артём <coshurin.art@yandex.ru>
2023-01-19 16:16:13 +0300

    final changes after review

--- homework.py
+++ homework.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
@@ -129,7 +130,7 @@ class Swimming(Training):
                 * self.weight * self.duration)
 
 
-def read_package(workout_type: str, data: list[int]) -> Training:
+def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
     training_types = {
         'SWM': Swimming,
